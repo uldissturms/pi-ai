b516fe8678e5517ef0c90df49d87ee47e6a21163
Uldis Sturms <uldis.sturms@fil.com>
2018-09-26 14:02:17 +0100

    chore: extract topic & add logging to iot spike

--- talk-to-bot/iot.py
+++ talk-to-bot/iot.py
@@ -2,9 +2,12 @@ import paho.mqtt.client as mqtt
 from urllib.parse import urlparse
 from os import getenv
 
+def on_log(client, userdata, level, buf):
+    print(client, userdata, level, buf)
+
 def on_connect(client, userdata, flags, rc):
     print('[iot] connected')
-    client.subscribe('test-iot')
+    client.subscribe(getenv('MQTT_TOPIC'))
 
 def on_message(client, _, msg):
     print('[iot] message received {}'.format(msg))
@@ -15,6 +18,7 @@ def connect(ws_url):
         'Host': '{0:s}'.format(urlparts.netloc),
     }
     client = mqtt.Client(transport='websockets')
+    client.on_log = on_log
     client.on_connect = on_connect
     client.on_message = on_message
 
